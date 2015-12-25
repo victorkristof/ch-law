7b188eb9dc773c76fa79ab9f0724c2f99545a14a
Victor Kristof <v.kristof@me.com>
2015-12-25 12:47:15 +0100

    Writing html to file instead of binary.

--- downloads.py
+++ downloads.py
@@ -1,9 +1,9 @@
 import requests
-import pickle
 import os
 from progressbar import ProgressBar
 
 droit_interne_data_dir = 'data/droit-interne'
+themes_dir = droit_interne_data_dir + '/themes'
 
 
 def get_html(url):
@@ -16,22 +16,22 @@ def get_html(url):
 
 def save_html(html, filename):
     if html:
-        with open(filename, 'wb') as f:
-            pickle.dump(html, f)
+        with open(filename, 'w') as f:
+            f.write(html)
 
 
 if __name__ == '__main__':
-    print('Downloading "Droit interne" entry points...')
-    os.mkdir(droit_interne_data_dir)
+    print('Downloading "Droit interne" themes...')
+    os.mkdir(themes_dir)
     template_url = 'https://www.admin.ch/opc/fr/classified-compilation/%s.html'
     bar = ProgressBar(81)
     # Get main themes
     for i in range(1, 10):
         html = get_html(template_url % i)
-        law_folder = '%s/%s' % (droit_interne_data_dir, i)
-        filename = law_folder+ '.html'
+        law_folder = '%s/%s' % (themes_dir, i)
+        filename = law_folder + '.html'
         save_html(html, filename)
-        os.mkdir('%s/%s' % (droit_interne_data_dir, i))
+        os.mkdir('%s/%s' % (themes_dir, i))
         for j in range(0, 10):
             law_id = i * 10 + j
             url = template_url % law_id
